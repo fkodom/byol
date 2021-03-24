c19a98883980a4f817c74c6769488d390580fd9e
Frank Odom <frank.odom.iii@gmail.com>
2021-03-24 10:25:12 -0500

    Refactoring to avoid 'deepcopy' for creating target networks.

---
+++ src/extractor.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+import torch
+from torch import nn, Tensor
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(
+        self,
+        model: nn.Module,
+        projection_size: int = 256,
+        hidden_size: int = 4096,
+        layer: Union[str, int] = -2,
+    ):
+        super().__init__()
+        self.model = model
+        self.projection_size = projection_size
+        self.hidden_size = hidden_size
+        self.layer = layer
+
+        self._extracted = torch.empty(0)
+        self._register_hook()
+
+    def _hook(self, _, __, output):
+        self._extracted = output.flatten(start_dim=1)
+
+    def _register_hook(self):
+        if isinstance(self.layer, str):
+            layer = dict([*self.model.named_modules()])[self.layer]
+        else:
+            layer = list(self.model.children())[self.layer]
+
+        layer.register_forward_hook(self._hook)
+
+    def forward(self, x: Tensor) -> Tensor:
+        _ = self.model(x)
+        return self._extracted
